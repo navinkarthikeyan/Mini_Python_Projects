0c44fb5df3ecfedf07652420d094d02f78d87a52
Navin Karthikeyan <52325703+navinkarthikeyan@users.noreply.github.com>
2023-04-27 17:21:50 +0530

    Update ProfanityCensor.py

--- ProfanityCensor.py
+++ ProfanityCensor.py
@@ -1,3 +1,4 @@
+#first you have to install better_profanity library by"pip install better_profanity"
 from better_profanity import profanity
 text = "shit ! I left my keys back in the car "
 censored = profanity.censor(text)
