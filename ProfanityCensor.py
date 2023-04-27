b1ae6fa812b8b7b943889509b205f2f5acffc093
Navin Karthikeyan <52325703+navinkarthikeyan@users.noreply.github.com>
2023-04-27 17:23:47 +0530

    Update ProfanityCensor.py

--- ProfanityCensor.py
+++ ProfanityCensor.py
@@ -1,5 +1,5 @@
 #first you have to install better_profanity library by"pip install better_profanity"
 from better_profanity import profanity
-text = text = input("Enter Text :") #for example enter "shit ! I left my keys back in the car "
+text = input("Enter Text :") #for example enter "shit ! I left my keys back in the car "
 censored = profanity.censor(text)
 print(censored)
